a4f9b7a924f45458fb10a6ebabd51a6836dcb352
funnbot <22226942+funnbot@users.noreply.github.com>
2025-10-01 17:30:50 -0700

    look how easy it is to copy paste from figma Granted, it isn't interactive or responsive.

---
+++ src/lib/ExampleWireframe.svelte
@@ -0,0 +1,488 @@
+<div
+	class="inline-flex flex-col items-start justify-start overflow-hidden bg-white outline outline-4 outline-gray-700"
+>
+	<div class="flex w-[1512px] flex-col items-start justify-start overflow-hidden bg-white">
+		<div class="inline-flex items-start justify-between self-stretch px-24 py-6">
+			<div
+				class="flex items-start justify-start gap-2.5 overflow-hidden rounded bg-gray-700 px-10 py-3"
+			>
+				<div class="justify-start font-['Inter'] text-base leading-normal font-bold text-white">
+					Logo
+				</div>
+			</div>
+			<div class="flex items-start justify-start gap-8">
+				<div
+					data-divider="False"
+					data-tab-1="true"
+					data-tab-2="true"
+					data-tab-3="true"
+					data-tab-4="true"
+					data-tab-5="true"
+					data-tab-6="true"
+					class="flex items-start justify-end gap-8"
+				>
+					<div
+						data-active="True"
+						data-icon-left="true"
+						data-icon-right="false"
+						class="flex items-center justify-start gap-2 self-stretch border-b-2 border-gray-700 py-3"
+					>
+						<div class="relative h-4 w-4 overflow-hidden">
+							<div class="absolute top-[1.33px] left-[1.33px] h-3.5 w-3.5 bg-gray-700"></div>
+						</div>
+						<div
+							class="justify-start text-center font-['Inter'] text-base leading-normal font-bold text-gray-700"
+						>
+							About
+						</div>
+					</div>
+					<div
+						data-active="False"
+						data-icon-left="true"
+						data-icon-right="false"
+						class="flex items-center justify-start gap-2 self-stretch py-3"
+					>
+						<div class="relative h-4 w-4 overflow-hidden">
+							<div class="absolute top-[1.33px] left-[1.33px] h-3.5 w-3.5 bg-slate-500"></div>
+						</div>
+						<div
+							class="justify-start text-center font-['Inter'] text-base leading-normal font-bold text-slate-500"
+						>
+							Events
+						</div>
+					</div>
+					<div
+						data-active="False"
+						data-icon-left="true"
+						data-icon-right="false"
+						class="flex items-center justify-start gap-2 self-stretch py-3"
+					>
+						<div class="relative h-4 w-4 overflow-hidden">
+							<div class="absolute top-[1.33px] left-[1.33px] h-3.5 w-3.5 bg-slate-500"></div>
+						</div>
+						<div
+							class="justify-start text-center font-['Inter'] text-base leading-normal font-bold text-slate-500"
+						>
+							Team
+						</div>
+					</div>
+					<div
+						data-active="False"
+						data-icon-left="true"
+						data-icon-right="false"
+						class="flex items-center justify-start gap-2 self-stretch py-3"
+					>
+						<div class="relative h-4 w-4 overflow-hidden">
+							<div class="absolute top-[1.33px] left-[1.33px] h-3.5 w-3.5 bg-slate-500"></div>
+						</div>
+						<div
+							class="justify-start text-center font-['Inter'] text-base leading-normal font-bold text-slate-500"
+						>
+							Sponsors
+						</div>
+					</div>
+					<div
+						data-active="False"
+						data-icon-left="true"
+						data-icon-right="false"
+						class="flex items-center justify-start gap-2 self-stretch py-3"
+					>
+						<div class="relative h-4 w-4 overflow-hidden">
+							<div class="absolute top-[1.33px] left-[1.33px] h-3.5 w-3.5 bg-slate-500"></div>
+						</div>
+						<div
+							class="justify-start text-center font-['Inter'] text-base leading-normal font-bold text-slate-500"
+						>
+							Tab
+						</div>
+					</div>
+					<div
+						data-active="False"
+						data-icon-left="true"
+						data-icon-right="false"
+						class="flex items-center justify-start gap-2 self-stretch py-3"
+					>
+						<div class="relative h-4 w-4 overflow-hidden">
+							<div class="absolute top-[1.33px] left-[1.33px] h-3.5 w-3.5 bg-slate-500"></div>
+						</div>
+						<div
+							class="justify-start text-center font-['Inter'] text-base leading-normal font-bold text-slate-500"
+						>
+							Tab
+						</div>
+					</div>
+				</div>
+				<div class="flex items-start justify-start gap-4">
+					<div
+						data-icon-left="false"
+						data-icon-right="false"
+						data-label="true"
+						data-size="Medium"
+						data-↳-style="Outline"
+						class="flex items-center justify-center gap-2 overflow-hidden rounded-md px-5 py-3 outline outline-2 outline-offset-[-2px] outline-gray-700"
+					>
+						<div
+							class="justify-start font-['Inter'] text-base leading-normal font-bold text-gray-700"
+						>
+							Button
+						</div>
+					</div>
+					<div
+						data-icon-left="false"
+						data-icon-right="false"
+						data-label="true"
+						data-size="Medium"
+						data-↳-style="Solid"
+						class="bg-primary-100 flex items-center justify-center gap-2 overflow-hidden rounded-md px-5 py-3"
+					>
+						<div class="justify-start font-['Inter'] text-base leading-normal font-bold text-white">
+							Button
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="h-0.5 self-stretch bg-gray-900"></div>
+	</div>
+	<div
+		class="flex h-[884px] w-[1512px] flex-col items-center justify-center bg-slate-100 p-24 shadow-[inset_0px_-2px_0px_0px_rgba(45,54,71,1.00)]"
+	>
+		<div class="flex w-[640px] flex-col items-center justify-start gap-10">
+			<div
+				data-2-lines="True"
+				data-centered="True"
+				data-full-width="True"
+				data-size="XXL"
+				class="flex flex-col items-center justify-start gap-4 self-stretch"
+			>
+				<div class="h-16 self-stretch rounded-[3px] bg-slate-500"></div>
+				<div class="inline-flex h-16 items-start justify-start self-stretch">
+					<div class="flex-1 self-stretch rounded-[3px]"></div>
+					<div class="flex-1 self-stretch rounded-tl-[3px] rounded-bl-[3px] bg-slate-500"></div>
+					<div class="flex-1 self-stretch bg-slate-500"></div>
+					<div class="flex-1 self-stretch rounded-tr-[3px] rounded-br-[3px] bg-slate-500"></div>
+					<div class="flex-1 self-stretch rounded-[3px]"></div>
+				</div>
+			</div>
+			<div
+				data-centered="True"
+				data-headline="False"
+				data-lines="5"
+				data-paragraphs="1"
+				class="flex flex-col items-center justify-start self-stretch"
+			>
+				<div class="flex flex-col items-start justify-start gap-2 self-stretch">
+					<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+					<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+					<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+					<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+					<div class="inline-flex items-start justify-start self-stretch">
+						<div class="h-4 flex-1 rounded-[3px]"></div>
+						<div class="h-4 flex-1 rounded-tl-[3px] rounded-bl-[3px] bg-slate-400"></div>
+						<div class="h-4 flex-1 rounded-tr-[3px] rounded-br-[3px] bg-slate-400"></div>
+						<div class="h-4 flex-1 rounded-[3px]"></div>
+					</div>
+				</div>
+			</div>
+			<div
+				data-icon="None"
+				data-style="Primary (Dark)"
+				class="flex flex-col items-start justify-start"
+			>
+				<div
+					data-icon-left="false"
+					data-icon-right="false"
+					data-label="true"
+					data-size="Large"
+					data-↳-style="Solid"
+					class="inline-flex items-center justify-center gap-2 overflow-hidden rounded-md bg-gray-700 px-6 py-4"
+				>
+					<div class="justify-start font-['Inter'] text-lg leading-normal font-bold text-white">
+						Join Us
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div
+		class="flex w-[1512px] flex-col items-center justify-start gap-10 overflow-hidden bg-white px-24 py-28"
+	>
+		<div
+			data-centered="True"
+			data-headline="True"
+			data-lines="1"
+			data-paragraphs="1"
+			class="flex flex-col items-start justify-start gap-6 self-stretch px-64"
+		>
+			<div
+				data-2-lines="False"
+				data-centered="True"
+				data-full-width="False"
+				data-size="L"
+				class="flex flex-col items-start justify-start gap-4 self-stretch"
+			>
+				<div class="inline-flex h-12 items-start justify-start self-stretch">
+					<div class="flex-1 self-stretch rounded-[3px]"></div>
+					<div class="flex-1 self-stretch rounded-tl-[3px] rounded-bl-[3px] bg-slate-500"></div>
+					<div class="flex-1 self-stretch bg-slate-500"></div>
+					<div class="flex-1 self-stretch rounded-tr-[3px] rounded-br-[3px] bg-slate-500"></div>
+					<div class="flex-1 self-stretch rounded-[3px]"></div>
+				</div>
+			</div>
+			<div class="flex flex-col items-start justify-start gap-2 self-stretch">
+				<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+			</div>
+		</div>
+		<div data-columns="3" class="inline-flex items-start justify-start gap-6 self-stretch pt-10">
+			<div
+				data-body-content="Placeholder"
+				data-button="Off"
+				data-headline="Placeholder"
+				data-image="Above Headline"
+				class="inline-flex flex-1 flex-col items-start justify-start overflow-hidden rounded-lg bg-white outline outline-2 outline-offset-[-2px] outline-gray-700"
+			>
+				<div
+					data-background="On"
+					data-frame="On"
+					class="relative h-56 self-stretch overflow-hidden bg-slate-100 outline outline-2 outline-offset-[-2px] outline-gray-700"
+				>
+					<div class="absolute top-[89px] left-[189px] h-10 w-10 overflow-hidden">
+						<div class="absolute top-[3.33px] left-[3.33px] h-8 w-8 bg-gray-700"></div>
+					</div>
+				</div>
+				<div class="flex flex-col items-start justify-start gap-6 self-stretch p-8">
+					<div class="flex flex-col items-start justify-start gap-6 self-stretch">
+						<div
+							data-2-lines="False"
+							data-centered="False"
+							data-full-width="False"
+							data-size="M"
+							class="flex flex-col items-start justify-start gap-3 self-stretch"
+						>
+							<div class="inline-flex h-10 items-start justify-start self-stretch">
+								<div
+									class="flex-1 self-stretch rounded-tl-[3px] rounded-bl-[3px] bg-slate-500"
+								></div>
+								<div
+									class="flex-1 self-stretch rounded-tr-[3px] rounded-br-[3px] bg-slate-500"
+								></div>
+								<div class="flex-1 self-stretch rounded-[3px]"></div>
+							</div>
+						</div>
+						<div
+							data-centered="False"
+							data-headline="False"
+							data-lines="4"
+							data-paragraphs="1"
+							class="flex flex-col items-start justify-start self-stretch"
+						>
+							<div class="flex flex-col items-start justify-start gap-2 self-stretch">
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="inline-flex items-start justify-start self-stretch">
+									<div class="h-4 flex-1 rounded-tl-[3px] rounded-bl-[3px] bg-slate-400"></div>
+									<div class="h-4 flex-1 rounded-tr-[3px] rounded-br-[3px] bg-slate-400"></div>
+									<div class="h-4 flex-1 rounded-[3px]"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				data-body-content="Placeholder"
+				data-button="Off"
+				data-headline="Placeholder"
+				data-image="Above Headline"
+				class="inline-flex flex-1 flex-col items-start justify-start self-stretch overflow-hidden rounded-lg bg-white outline outline-2 outline-offset-[-2px] outline-gray-700"
+			>
+				<div
+					data-background="On"
+					data-frame="On"
+					class="relative flex-1 self-stretch overflow-hidden bg-slate-100 outline outline-2 outline-offset-[-2px] outline-gray-700"
+				>
+					<div class="absolute top-[89px] left-[189px] h-10 w-10 overflow-hidden">
+						<div class="absolute top-[3.33px] left-[3.33px] h-8 w-8 bg-gray-700"></div>
+					</div>
+				</div>
+				<div class="flex flex-col items-start justify-start gap-6 self-stretch p-8">
+					<div class="flex flex-col items-start justify-start gap-6 self-stretch">
+						<div
+							data-2-lines="False"
+							data-centered="False"
+							data-full-width="False"
+							data-size="M"
+							class="flex flex-col items-start justify-start gap-3 self-stretch"
+						>
+							<div class="inline-flex h-10 items-start justify-start self-stretch">
+								<div
+									class="flex-1 self-stretch rounded-tl-[3px] rounded-bl-[3px] bg-slate-500"
+								></div>
+								<div
+									class="flex-1 self-stretch rounded-tr-[3px] rounded-br-[3px] bg-slate-500"
+								></div>
+								<div class="flex-1 self-stretch rounded-[3px]"></div>
+							</div>
+						</div>
+						<div
+							data-centered="False"
+							data-headline="False"
+							data-lines="4"
+							data-paragraphs="1"
+							class="flex flex-col items-start justify-start self-stretch"
+						>
+							<div class="flex flex-col items-start justify-start gap-2 self-stretch">
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="inline-flex items-start justify-start self-stretch">
+									<div class="h-4 flex-1 rounded-tl-[3px] rounded-bl-[3px] bg-slate-400"></div>
+									<div class="h-4 flex-1 rounded-tr-[3px] rounded-br-[3px] bg-slate-400"></div>
+									<div class="h-4 flex-1 rounded-[3px]"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				data-body-content="Placeholder"
+				data-button="Off"
+				data-headline="Placeholder"
+				data-image="Above Headline"
+				class="inline-flex flex-1 flex-col items-start justify-start self-stretch overflow-hidden rounded-lg bg-white outline outline-2 outline-offset-[-2px] outline-gray-700"
+			>
+				<div
+					data-background="On"
+					data-frame="On"
+					class="relative flex-1 self-stretch overflow-hidden bg-slate-100 outline outline-2 outline-offset-[-2px] outline-gray-700"
+				>
+					<div class="absolute top-[89px] left-[189px] h-10 w-10 overflow-hidden">
+						<div class="absolute top-[3.33px] left-[3.33px] h-8 w-8 bg-gray-700"></div>
+					</div>
+				</div>
+				<div class="flex flex-col items-start justify-start gap-6 self-stretch p-8">
+					<div class="flex flex-col items-start justify-start gap-6 self-stretch">
+						<div
+							data-2-lines="False"
+							data-centered="False"
+							data-full-width="False"
+							data-size="M"
+							class="flex flex-col items-start justify-start gap-3 self-stretch"
+						>
+							<div class="inline-flex h-10 items-start justify-start self-stretch">
+								<div
+									class="flex-1 self-stretch rounded-tl-[3px] rounded-bl-[3px] bg-slate-500"
+								></div>
+								<div
+									class="flex-1 self-stretch rounded-tr-[3px] rounded-br-[3px] bg-slate-500"
+								></div>
+								<div class="flex-1 self-stretch rounded-[3px]"></div>
+							</div>
+						</div>
+						<div
+							data-centered="False"
+							data-headline="False"
+							data-lines="4"
+							data-paragraphs="1"
+							class="flex flex-col items-start justify-start self-stretch"
+						>
+							<div class="flex flex-col items-start justify-start gap-2 self-stretch">
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="h-4 self-stretch rounded-[3px] bg-slate-400"></div>
+								<div class="inline-flex items-start justify-start self-stretch">
+									<div class="h-4 flex-1 rounded-tl-[3px] rounded-bl-[3px] bg-slate-400"></div>
+									<div class="h-4 flex-1 rounded-tr-[3px] rounded-br-[3px] bg-slate-400"></div>
+									<div class="h-4 flex-1 rounded-[3px]"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div
+			data-fat="false"
+			data-padding-bottom="true"
+			data-padding-top="true"
+			class="flex flex-col items-center justify-center self-stretch py-6"
+		>
+			<div class="h-0.5 self-stretch bg-gray-700"></div>
+		</div>
+		<div
+			data-icon="None"
+			data-style="Primary (Dark)"
+			class="flex flex-col items-center justify-center self-stretch"
+		>
+			<div
+				data-icon-left="false"
+				data-icon-right="false"
+				data-label="true"
+				data-size="Large"
+				data-↳-style="Solid"
+				class="inline-flex items-center justify-center gap-2 overflow-hidden rounded-md bg-gray-700 px-6 py-4"
+			>
+				<div class="justify-start font-['Inter'] text-lg leading-normal font-bold text-white">
+					See More
+				</div>
+			</div>
+		</div>
+	</div>
+	<div
+		data-size="Desktop"
+		class="flex w-[1512px] flex-col items-center justify-start gap-10 overflow-hidden bg-slate-500 px-24 py-28"
+	>
+		<div data-columns="1" class="inline-flex h-56 items-start justify-start gap-6 self-stretch">
+			<div
+				class="relative flex-1 self-stretch overflow-hidden rounded-lg bg-white outline outline-2 outline-offset-[-2px] outline-slate-300"
+			>
+				<div
+					class="absolute top-[90px] left-[628px] justify-center text-center font-['Inter'] text-2xl leading-10 font-normal text-slate-400"
+				>
+					Slot
+				</div>
+			</div>
+		</div>
+	</div>
+	<div
+		data-size="Desktop"
+		class="flex w-[1512px] flex-col items-start justify-center gap-8 overflow-hidden bg-gray-900 px-24 py-16"
+	>
+		<div
+			class="inline-flex items-start justify-start gap-2.5 overflow-hidden rounded bg-gray-700 px-10 py-3"
+		>
+			<div class="justify-start font-['Inter'] text-base leading-normal font-bold text-white">
+				Logo
+			</div>
+		</div>
+		<div class="inline-flex items-start justify-between self-stretch">
+			<div class="justify-start font-['Inter'] text-sm leading-tight font-normal text-white">
+				Copyright 2022 Company Name.
+			</div>
+			<div class="flex items-start justify-start gap-8">
+				<div
+					class="justify-start text-center font-['Inter'] text-sm leading-tight font-normal text-slate-400"
+				>
+					Privacy Policy
+				</div>
+				<div
+					class="justify-start text-center font-['Inter'] text-sm leading-tight font-normal text-slate-400"
+				>
+					Terms & Conditions
+				</div>
+				<div
+					class="justify-start text-center font-['Inter'] text-sm leading-tight font-normal text-slate-400"
+				>
+					Cookie Policy
+				</div>
+				<div
+					class="justify-start text-center font-['Inter'] text-sm leading-tight font-normal text-slate-400"
+				>
+					Contact
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import ExampleWireframe from "$lib/ExampleWireframe.svelte";
+</script>
+
+<ExampleWireframe />
