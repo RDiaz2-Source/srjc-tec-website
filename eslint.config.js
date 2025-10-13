cf4c8fc338e64d42b37116db070b4139d3521012
funnbot <22226942+funnbot@users.noreply.github.com>
2025-10-13 13:55:24 -0700

    chore: Add a tailwind class list formatter + linter.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
+import svelteParser from 'svelte-eslint-parser';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -19,23 +21,38 @@ export default defineConfig(
 	...svelte.configs.prettier,
 	{
 		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
+			globals: { ...globals.browser, ...globals.node },
 		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
+			'no-undef': 'off',
+		},
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
+			parser: svelteParser,
 			parserOptions: {
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
+				svelteConfig,
+			},
+		},
+	},
+	{
+		plugins: {
+			'better-tailwindcss': eslintPluginBetterTailwindcss,
+		},
+		rules: {
+			// enable all recommended rules to report a warning
+			...eslintPluginBetterTailwindcss.configs['recommended-warn'].rules,
+		},
+		settings: {
+			'better-tailwindcss': {
+				entryPoint: 'src/app.css',
+			},
+		},
+	},
 );
